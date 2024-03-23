d2493554bc586691f0c4e7969f51051b5defd2ce
FredPotter <ftpotter1993@gmail.com>
2024-03-23 20:21:10 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import {Route, Routes} from 'react-router-dom';
+import Login from './Pages/Login/Login';
+// import Logout from './Pages/Logout/Logout'; // Предполагается, что вы создали этот компонент
+// import Account from './Pages/Account/Account'; // Предполагается, что вы создали этот компонент
+import AppBar from "./Pages/HomeComponents/AppBar/AppBar";
+import theme from "./Pages/HomeComponents/Theme/Theme";
+import {ThemeProvider} from '@mui/material/styles';
+import SignUp from "./Pages/SignUp/SignUp";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <ThemeProvider theme={theme}>
+                <Routes>
+                    <Route path="/Main" element={<AppBar/>}/>
+                    <Route path="/Login" element={<Login/>}/>
+                    <Route path="/SignUp" element={<SignUp/>}/>
+                    {/*<Route path="/Logout" element={<Logout/>}/>*/}
+                    {/*<Route path="/Account" element={<Account/>}/>*/}
+                </Routes>
+            </ThemeProvider>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Pages/HomeComponents/Theme/Theme.jsx
@@ -0,0 +1,16 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#574C4A',
+            contrastText: '#edb8af',
+        },
+        secondary: {
+            main: '#806F6C',
+            contrastText: '#edb8af',
+        },
+    },
+});
+
+export default theme;
